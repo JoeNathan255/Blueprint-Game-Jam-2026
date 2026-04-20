c901c3fcc2ebff2037726d195d4750ad5cd6d34f
aptheriault2005 <aptheriault2005@gmail.com>
2026-04-19 23:01:15 -0400

    Start work on layout

---
+++ Assets/Scripts/AnimTest.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimTest : MonoBehaviour
+{
+    public Vector2 inputVec;
+    Animator entityAnimator;
+
+    void Start()
+    {
+        entityAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movX = Input.GetAxis("Horizontal");
+        float movY = Input.GetAxis("Vertical");
+        inputVec = new Vector2(movX, movY).normalized;
+
+        UpdateAnimation(inputVec);
+    }
+
+    private void UpdateAnimation(Vector2 normalizedInputVec)
+    {
+        if (normalizedInputVec.x > 0 || normalizedInputVec.y > 0 || normalizedInputVec.x < 0 || normalizedInputVec.y < 0)
+        {
+            entityAnimator.SetBool("IsWalking", true);
+            entityAnimator.SetFloat("InputX", normalizedInputVec.x);
+            entityAnimator.SetFloat("InputY", normalizedInputVec.y);
+        }
+        else
+        {
+            entityAnimator.SetBool("IsWalking", false);
+        }
+    }
+}
